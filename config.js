1b627f5baabd6cdc554f5ec979a722760c7954a1
Yacine Rezgui <rezgui.y@gmail.com>
2014-04-13 20:00:35 +0100

    Simplify fonts config and update paths to make them relative

--- config.js
+++ config.js
@@ -2,26 +2,23 @@ var config = module.exports = {};
 
 config.externalFiles = {
   styles: [
-    'vendor/ionic/release/css/ionic.css'
+    './vendor/ionic/release/css/ionic.css'
   ],
   scripts: [],
   views: [],
   images: [],
   fonts: [
-    'vendor/ionic/release/fonts/ionicons.eot',
-    'vendor/ionic/release/fonts/ionicons.svg',
-    'vendor/ionic/release/fonts/ionicons.ttf',
-    'vendor/ionic/release/fonts/ionicons.woff'
+    './vendor/ionic/release/fonts/**/*'
   ]
 };
 
 config.path = {
-  build:      'www',
-  styles:     ['src/styles/**/*.less'],
-  scripts:    ['src/scripts/**/*.js'],
-  views:      ['src/views/**/*.jade'],
-  fonts:      ['src/fonts/**/*'],
-  images:     ['src/images/**/*'],
-  resources:  ['src/resources/**/*'],
-  configs:    ['src/config/**/*']
+  build:      './www',
+  styles:     ['./src/styles/**/*.less'],
+  scripts:    ['./src/scripts/**/*.js'],
+  views:      ['./src/views/**/*.jade'],
+  fonts:      ['./src/fonts/**/*'],
+  images:     ['./src/images/**/*'],
+  resources:  ['./src/resources/**/*'],
+  configs:    ['./src/config/**/*']
 };

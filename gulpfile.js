1de4096a2332598d12d31307e3b6ee46291a739a
Yacine Rezgui <rezgui.y@gmail.com>
2014-04-14 10:04:15 +0100

    Big update of gulp file (need to avoid a big one like that in the future)

--- gulpfile.js
+++ gulpfile.js
@@ -5,79 +5,136 @@ var browserify  = require('gulp-browserify');
 var jade        = require('gulp-jade');
 var less        = require('gulp-less');
 var es          = require('event-stream');
+var path        = require('path');
+var Q           = require('q');
 
-gulp.task('clean', function(){
+function toPromise(stream) {
+  var deferred = Q.defer();
+
+  stream.on('end', function end() {
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+}
+
+function cleanTask() {
   return gulp.src(config.path.build, {read: false})
              .pipe(clean());
-});
+}
 
-gulp.task('config', function() {
+function configTask() {
   return gulp.src(config.path.configs)
              .pipe(gulp.dest(config.path.build));
-});
+}
 
-gulp.task('resources', function() {
+function resourcesTask() {
   return gulp.src(config.path.resources)
              .pipe(gulp.dest(path.join(config.path.build, 'res')));
-});
+}
 
-gulp.task('scripts', function() {
-  // Single entry point to browserify
+function scriptsTask() {
   return gulp.src('src/scripts/app.js')
              .pipe(browserify({
                 insertGlobals : true,
                 debug : true
              }))
              .pipe(gulp.dest(path.join(config.path.build, 'js')));
-});
+}
 
-gulp.task('fonts', function() {
+function fontsTask() {
   return es.concat(
     gulp.src(config.path.fonts),
     gulp.src(config.externalFiles.fonts)
 
   ).pipe(gulp.dest(path.join(config.path.build, 'fonts')));
-});
+}
 
-gulp.task('images', function() {
+function imagesTask() {
   return gulp.src(config.path.images)
              .pipe(gulp.dest(path.join(config.path.build, 'img')));
-});
+}
 
-gulp.task('styles', function(){
+function stylesTask() {
   return es.concat(
     gulp.src('src/styles/app.less').pipe(less()),
     gulp.src(config.externalFiles.styles)
 
   ).pipe(gulp.dest(path.join(config.path.build, 'css')));
+}
+
+function viewsTask() {
+  return gulp.src(['src/views/templates/**/*.jade'])
+             .pipe(jade({pretty: true}))
+             .pipe(gulp.dest(path.join(config.path.build, 'tpl')));
+}
+
+function indexTask() {
+  return gulp.src('src/views/index.jade')
+             .pipe(jade({pretty: true}))
+             .pipe(gulp.dest(config.path.build));
+}
+
+gulp.task('clean', function(){
+  return cleanTask();
+});
+
+gulp.task('config', function() {
+  return configTask();
+});
+
+gulp.task('resources', function() {
+  return resourcesTask();
+});
+
+gulp.task('scripts', function() {
+  return scriptsTask();
+});
+
+gulp.task('fonts', function() {
+  return fontsTask();
+});
+
+gulp.task('images', function() {
+  return imagesTask();
+});
+
+gulp.task('styles', function(){
+  return stylesTask();
 });
 
 gulp.task('views', function(){
-  return gulp.src(['src/views/templates/**/*.jade'])
-             .pipe(jade())
-             .pipe(gulp.dest(path.join(config.path.build, 'tpl')));
+  return viewsTask();
 });
 
 gulp.task('index', function(){
-  return gulp.src('src/views/index.jade')
-             .pipe(jade())
-             .pipe(gulp.dest(config.path.build));
+  return indexTask();
 });
 
 gulp.task('watch', ['clean'], function(){
 
-  gulp.run('scripts', 'styles', 'views', 'index', 'images', 'fonts', 'resources', 'config',
-    function watch() {
+  return Q.all([
+    toPromise(configTask()),
+    toPromise(resourcesTask()),
+    toPromise(scriptsTask()),
+    toPromise(fontsTask()),
+    toPromise(imagesTask()),
+    toPromise(stylesTask()),
+    toPromise(viewsTask()),
+    toPromise(indexTask())
+    
+  ]).then(function watch() {
 
-      gulp.watch(config.path.styles,  ['styles']);
-      gulp.watch(config.path.scripts, ['scripts']);
-      gulp.watch(config.path.images,  ['images']);
-      gulp.watch(config.path.fonts,   ['fonts']);
-      gulp.watch(config.path.configs, ['config']);
-      gulp.watch(config.path.views,   ['views', 'index']);
-    },
-    function initialTasks() {
       console.log('watching...');
-    }
-  );
+
+      gulp.watch(config.path.styles,    ['styles']);
+      gulp.watch(config.path.scripts,   ['scripts']);
+      gulp.watch(config.path.images,    ['images']);
+      gulp.watch(config.path.fonts,     ['fonts']);
+      gulp.watch(config.path.configs,   ['config']);
+      gulp.watch(config.path.resources, ['resources']);
+      gulp.watch(config.path.views,     ['views', 'index']);
+  });
 });
+
+gulp.task('default', ['watch']);

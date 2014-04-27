cc90373680416fa875b0f718a23df6d877866241
Yacine Rezgui <rezgui.y@gmail.com>
2014-04-28 00:32:41 +0100

    (#12) Add dependencies page

---
+++ src/app/tabs/home/dependencies/dependencies.js
@@ -0,0 +1,18 @@
+angular.module('states.tabs.home.dependencies', [])
+  
+  .config(['$stateProvider', function config($stateProvider) {
+    $stateProvider.state('tabs.home.dependencies', {
+      url: '/dependencies',
+      views: {
+        'tab-home@tabs': {
+          templateUrl: 'app/tabs/home/dependencies/dependencies.html',
+          controller: 'DependenciesCtrl'
+        }
+      }
+    });
+  }])
+
+  .controller('DependenciesCtrl', ['$scope', function DependenciesCtrl($scope) {
+
+    console.log('Dependencies controller loaded', $scope);
+  }]);

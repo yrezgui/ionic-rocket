d2be8ca8e6f40cb22f8f1e103b895a8a74a39913
Yacine Rezgui <rezgui.y@gmail.com>
2014-05-05 18:53:06 +0100

    (#18) Create the commits controller + view

--- src/app/tabs/commits/commits.js
+++ src/app/tabs/commits/commits.js
@@ -13,7 +13,31 @@ angular.module('app.tabs.commits', ['components.github'])
   }])
 
 
-  .controller('CommitsCtrl', ['$scope', function CommitsCtrl($scope) {
+  .controller('CommitsCtrl', ['$scope', 'Github', function CommitsCtrl($scope, Github) {
 
     console.log('Commits controller loaded', $scope);
+
+    $scope.lastSha              = null;
+    $scope.commits              = [];
+    $scope.noMoreItemsAvailable = false;
+    
+    $scope.getCommits = function getCommits(noUiUpdate) {
+
+      Github.getLastCommits($scope.lastSha).then(function success(results) {
+
+        if(results.length) {
+          $scope.commits = _.uniq($scope.commits.concat(results), function uniq(commit) {
+            return commit.sha;
+          });
+
+          $scope.lastSha = results[results.length - 1].sha;
+        } else {
+          $scope.noMoreItemsAvailable = true;
+        }
+
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    };
+
+    $scope.getCommits();
   }]);
